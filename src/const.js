3c88617d89a712259c4001d5061197e022eeba35
Anvar Gulomov <anvar.gulomov.97@gmail.com>
2023-01-22 15:13:20 -0500

    env was created

--- src/const.js
+++ src/const.js
@@ -8,3 +8,8 @@ export const API = {
   VIDEOS: 'https://project-2-api.herokuapp.com/videos?api_key=d9321c15-a32c-46ab-a15d-07809df6aaef',
   VIDEO: 'https://project-2-api.herokuapp.com/videos/:id?api_key=d9321c15-a32c-46ab-a15d-07809df6aaef',
 };
+
+
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const ENDPOINT = `${BASE_URL}`;
